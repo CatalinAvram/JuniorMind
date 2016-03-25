db0d1711509afa2b47c43c9922378a9c7d3ee585
CatalinAvram <avram.catalin94@yahoo.com>
2016-03-26 01:11:42 +0200

    Calculate number of kilograms eaten

---
+++ JuniorMind/Goats/GoatsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+/*În X zile Y capre mănâncă Z kg fân. Câte kg de fân mănâncă Q capre în W zile?*/
+namespace Goats
+{
+    [TestClass]
+    public class GoatsTests
+    {
+        [TestMethod]
+        public void NumberOfKilograms()
+        {
+            int totalKilograms = FindNumberOfKilograms(5, 4);
+            Assert.AreEqual(20, totalKilograms);
+        }
+        int FindNumberOfKilograms(int numberOfGoats, int numberOfDays)
+        {
+            return numberOfGoats * numberOfDays;
+        }
+    }
+}
