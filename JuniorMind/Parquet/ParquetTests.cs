17894370e0b84a5a94d4d8bd84c71aaefdd31698
CatalinAvram <avram.catalin94@yahoo.com>
2016-03-27 17:34:41 +0300

    Calculate number of needed parquet pieces

---
+++ JuniorMind/Parquet/ParquetTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+/*Trebuie pus parchet laminat într-o cameră de N x M mp. Dimensiunea unei plăci de parchet e de A x B mp. Pierderile 
+sunt de 15%. De cât parchet e nevoie?*/
+
+namespace Parquet
+{
+    [TestClass]
+    public class ParquetTests
+    {
+        [TestMethod]
+        public void piecesNeeded()
+        {
+            Assert.AreEqual(14, CalculateParquetPieces(6, 4, 2, 1, 15));
+        }
+        decimal CalculateParquetPieces(int roomLength, int roomWidth, int pieceLength,  int pieceWidth, decimal losses)
+        {
+            int roomDimension = roomLength * roomWidth;
+            int pieceDimension = pieceLength * pieceWidth;
+            decimal numberOfPieces = roomDimension / pieceDimension;
+            decimal supplemantaryPieces = losses / 100 * numberOfPieces;
+
+            return Math.Round(numberOfPieces + supplemantaryPieces);
+        }
+    }
+}
