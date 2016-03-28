8261c07e0db4409048729331cc2cce64094913a8
CatalinAvram <avram.catalin94@yahoo.com>
2016-03-28 15:20:54 +0300

    Test for even weight value

---
+++ JuniorMind/Watermelon/WatermelonTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+/*Doi prieteni au cumpărat un pepene de X kg, și doresc să îl împartă.
+Singura lor dorință e ca fiecare să primească un număr par de kg din pepene.
+E ok și dacă nu primesc aceeași cantitate.
+Scrie un program care răspunde cu DA dacă pepenele poate fi împărțit cum își doresc cei doi prieteni, 
+și cu NU în caz contrar.*/
+
+namespace Watermelon
+{
+    [TestClass]
+    public class WatermelonTests
+    {
+        [TestMethod]
+        public void SplittedWatermelonYes()
+        {
+            String answer = checkIfCanBeSplittedInEvenParts(4);
+            Assert.AreEqual("DA", answer);
+        }
+        string checkIfCanBeSplittedInEvenParts(int watermelonWeight)
+        {
+            return (watermelonWeight % 2 == 0 ? "DA" : "NU");
+        }
+    }
+}
