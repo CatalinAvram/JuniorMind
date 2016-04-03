90fee4307d333cf740210202a962013c2dcc040b
CatalinAvram <avram.catalin94@yahoo.com>
2016-04-03 19:13:46 +0300

    Check for a phrase which is not panagram

---
+++ JuniorMind/Panagram/PanagramsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+/*
+Un panagram e o frază care conține toate literele din alfabet (englezesc).
+Scrie un program care determină dacă o frază e o panagram.
+
+Exemplu: The quick brown fox jumps over the lazy dog*/
+
+namespace Panagram
+{
+    [TestClass]
+    public class PanagramsTests
+    {
+        [TestMethod]
+        public void NotAPanagram()
+        {
+            Assert.AreEqual(false, IsAPanagram("I like IT"));
+        }
+
+        bool IsAPanagram(string phrase)
+        {
+            phrase.ToLower();
+            string phraseLetters = "";
+            for (int i = 0; i < phrase.Length; i++)
+                if (!phraseLetters.Contains(phrase[i].ToString()))
+                    phraseLetters = phraseLetters + phrase[i];
+            if (phraseLetters.Length == 26)
+                return true;
+            return false;
+        } 
+    }
+}
