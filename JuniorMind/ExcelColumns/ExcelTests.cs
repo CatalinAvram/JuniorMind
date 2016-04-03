01db1e6ef6f75eaa7de157b726c2b9d40f7ef908
CatalinAvram <avram.catalin94@yahoo.com>
2016-04-03 21:52:06 +0300

    Check for columns with a single letter

---
+++ JuniorMind/ExcelColumns/ExcelTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+/*Primele 26 de coloane din Excel sunt marcate cu literele alfabetului.
+După care continuă cu combinații de câte două litere, astfel coloana 27 este AA, 28 - AB, iar coloana 52 cu AZ.
+După ZZ, se continuă cu combinații de 3 litere.
+
+Dacă se dă numărul coloanei află care e combinația de litere corespunzătoare.*/
+namespace ExcelColumns
+{
+    [TestClass]
+    public class ExcelTests
+    {
+        [TestMethod]
+        public void OneLetterName()
+        {
+            Assert.AreEqual("C", GiveColumnCorrespondingString(3));
+        }
+
+        string GiveColumnCorrespondingString (int columnNumber)
+        {
+            string[] letters = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
+                                 "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            if (columnNumber <= 26)
+                return letters[columnNumber - 1];
+            else
+                return "";
+        } 
+    }
+}
