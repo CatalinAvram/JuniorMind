1cb937de52ee8d11c3929928bb3414a82d306d81
CatalinAvram <avram.catalin94@yahoo.com>
2016-04-03 23:33:30 +0300

    Compute the factorial of a number

--- JuniorMind/ExcelColumns/ExcelTests.cs
+++ JuniorMind/ExcelColumns/ExcelTests.cs
@@ -17,14 +17,37 @@ namespace ExcelColumns
             Assert.AreEqual("C", GiveColumnCorrespondingString(3));
         }
 
+        [TestMethod]
+        public void TwoLettersName()
+        {
+            Assert.AreEqual("BA", GiveColumnCorrespondingString(52));
+        }
+
+        [TestMethod]
+        public void FactorialNumber()
+        {
+            Assert.AreEqual(120, ComputeFactorialNumber(5));
+        }
+
         string GiveColumnCorrespondingString (int columnNumber)
         {
-            string[] letters = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N",
-                                 "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+            string[] letters = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+                                 "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
             if (columnNumber <= 26)
-                return letters[columnNumber - 1];
+                return letters[columnNumber - 1];       
             else
                 return "";
         } 
+
+        int ComputeFactorialNumber(int number)
+        {
+            if (number == 0)
+                return 1;
+
+            int factorial = 1;
+            for (int i = 1; i <= number; i++)
+                factorial *= i;
+            return factorial;
+        }
     }
 }
