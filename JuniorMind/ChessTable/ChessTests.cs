b47bd14300b9d089d12cd609c758da804b5f8b46
CatalinAvram <avram.catalin94@yahoo.com>
2016-04-03 13:54:41 +0300

    First letter is common for both strings

---
+++ JuniorMind/ChessTable/ChessTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ChessTable
+{
+    [TestClass]
+    public class ChessTests
+    {
+        [TestMethod]
+        public void ()
+        {
+
+        }
+        decimal CalculateNumberOfSquares(int Length)
+        {
+            return 0;
+        }
+    }
+}
---
+++ JuniorMind/CommonPrefixe/PrefixTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//Dacă ai două stringuri, găsește-le prefixul comun. Exemplu: "aaab" și "aaaabbaa" au prefixul comun "aaa"
+namespace CommonPrefixe
+{
+    [TestClass]
+    public class PrefixesTests
+    {
+        [TestMethod]
+        public void OneCommonLetter()
+        {
+            Assert.AreEqual("a", FindCommonPrefix("abba", "acca"));
+        }
+        string FindCommonPrefix(string firstString, string secondString)
+        {
+            string commonPrefix = "";
+            for (int i = 0; i < firstString.Length; i++)
+                if (firstString[i] == secondString[i])
+                    commonPrefix += firstString[i];
+                else
+                    break;
+            return commonPrefix;
+        }
+    }
+}
