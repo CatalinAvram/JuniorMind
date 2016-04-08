ea562f3e3a6e5cc4f6e6bbcb11bf4bb360874b78
CatalinAvram <avram.catalin94@yahoo.com>
2016-04-08 23:47:43 +0300

    3X3 Table

--- JuniorMind/ChessTable/ChessTests.cs
+++ JuniorMind/ChessTable/ChessTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+//Calculează câte pătrate se pot forma pe o tablă de șah de dimensiunea de 8 x 8.
+
 namespace ChessTable
 {
     [TestClass]
@@ -12,9 +14,23 @@ namespace ChessTable
             Assert.AreEqual(4, ComputeNumberOfSquares(2, 2));
         }
 
+        [TestMethod]
+        public void ThreeMultipliedByThreeTable()
+        {
+            Assert.AreEqual(13, ComputeNumberOfSquares(3,3));
+        }
+
         private int ComputeNumberOfSquares(int length, int width)
         {
-            return length * width;
+            int numberOfSquares = 1;
+            while(length > 1)
+            {
+                numberOfSquares += length * width;
+                length--;
+                width--;
+            }
+
+            return numberOfSquares - 1;
         }
     }
 }
