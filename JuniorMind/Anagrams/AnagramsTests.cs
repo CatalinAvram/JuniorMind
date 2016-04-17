3cb59bed90bd50e25df39617b4cd4f0c827ea89f
CatalinAvram <avram.catalin94@yahoo.com>
2016-04-17 11:53:04 +0300

    Test for more repeating letters

--- JuniorMind/Anagrams/AnagramsTests.cs
+++ JuniorMind/Anagrams/AnagramsTests.cs
@@ -13,9 +13,40 @@ namespace Anagrams
             Assert.AreEqual(2, ComputeNumberOfAnagrams("ab"));
         }
 
+        [TestMethod]
+        public void ThreeDistinctLetters()
+        {
+            Assert.AreEqual(6, ComputeNumberOfAnagrams("abc"));
+        }
+
+        [TestMethod]
+        public void TwoIdenticalLetters()
+        {
+            Assert.AreEqual(1, ComputeNumberOfAnagrams("aa"));
+        }
+
+        [TestMethod]
+        public void MoreRepeatingLetters()
+        {
+            Assert.AreEqual(6, ComputeNumberOfAnagrams("aabb"));
+        }
+
         int ComputeNumberOfAnagrams(string word)
         {
-            return ComputeFactorial(word.Length);
+            int productOfRepetitions = 1;
+            for (int c = 'a'; c < 'z'; c++)
+                productOfRepetitions *= ComputeFactorial(GetNumberOfRepetions(c, word));
+            return ComputeFactorial(word.Length) / productOfRepetitions;
+        }
+
+        private int GetNumberOfRepetions(int charToCheck, string word)
+        {
+            int repetitionsNumber = 0;
+            for (int i = 0; i < word.Length; i++)
+                if (charToCheck == word[i])
+                    repetitionsNumber++;
+            return repetitionsNumber;
+
         }
 
         private static int ComputeFactorial(int number)
