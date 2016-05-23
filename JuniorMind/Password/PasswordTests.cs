553cde55b353ef4dbb02121f772b610a9e393e56
CatalinAvram <avram.catalin94@yahoo.com>
2016-05-23 21:56:41 +0300

    Small letters, capitals and ciphers

--- JuniorMind/Password/PasswordTests.cs
+++ JuniorMind/Password/PasswordTests.cs
@@ -27,55 +27,61 @@ namespace Password
         }
     }
 
+    public enum CharType
+    {
+        SmallLetters, CapitalLetters, Ciphers
+    }
+
     [TestClass]
     public class PasswordTests
     {
         [TestMethod]
-        public void RandomPassword()
+        public void SmallAndCapitalLetters()
         {
             Password password = new Password(3, 2, 1, 0, 0, "");
             password.passwordArray = GeneratePassword(password);      
             Assert.AreEqual(true, CheckPassword(password));
         }
 
+        [TestMethod]
+        public void SmallCapitalAndCipher()
+        {
+            Password password = new Password(5, 2, 1, 2, 0, "");
+            password.passwordArray = GeneratePassword(password);
+            Assert.AreEqual(true, CheckPassword(password));
+        }
+
         string GeneratePassword(Password password)
         {
-            password.passwordArray = GenerateSmallLetters(password) + GenerateCapitalLetters(password);
+            password.passwordArray = AddChars(password, CharType.SmallLetters, password.nrOfSmallLetters) + AddChars(password, CharType.CapitalLetters, password.nrOfCapitalLetters) + AddChars(password, CharType.Ciphers, password.nrOfCiphers);
             return password.passwordArray;
         }
 
-        char GenerateRandomSmallLetter()
+        char GetRandomChar(CharType charType)
         {
-            Random random = new Random();          
-            int index = random.Next('a', 'z');
+            Random random = new Random();
+            int index = 0;
+            switch (charType)
+            {
+                case CharType.SmallLetters:
+                    index = random.Next('a', 'z');
+                    break;
+                case CharType.CapitalLetters:
+                    index = random.Next('A', 'Z');
+                    break;
+                case CharType.Ciphers:
+                    index = random.Next('0', '9');
+                    break;
+            }                   
             return  (char)(index);
         }
        
-        string GenerateSmallLetters(Password password)
-        {
-            int i = 0;  
-            while(i < password.nrOfSmallLetters)
+        string AddChars(Password password, CharType charType, int nrOfChars)
+        {                    
+            int i = 0;          
+            while (i < nrOfChars)
             {
-                password.passwordArray += GenerateRandomSmallLetter();
-                i++;
-            }
-            return password.passwordArray;
-        }
-
-        char GenerateRandomCapitalLetter()
-        {
-            Random random = new Random();
-            char c = new char();
-            int index = random.Next('A', 'Z');
-            return c = (char)(index);
-        }
-
-        string GenerateCapitalLetters(Password password)
-        {
-            int i = 0;
-            while (i < password.nrOfCapitalLetters)
-            {
-                password.passwordArray += GenerateRandomCapitalLetter();
+                password.passwordArray += GetRandomChar(charType);
                 i++;
             }
             return password.passwordArray;
@@ -85,17 +91,17 @@ namespace Password
         {            
             int smallLetters = 0;
             int capitalLetters = 0;
-            //int ciphers = 0;
+            int ciphers = 0;
             for(int i = 0; i < password.passwordArray.Length; i++)
             {
                 if (password.passwordArray[i] >= 'a' && password.passwordArray[i] <= 'z')
-                    smallLetters++;
+                    smallLetters++;                
                 if (password.passwordArray[i] >= 'A' && password.passwordArray[i] <= 'Z')
-                    capitalLetters++;
-               // if (password.passwordArray[i] >= 0 && password.passwordArray[i] <= 9)
-                  //  ciphers++;
+                    capitalLetters++;           
+                if (password.passwordArray[i] >= '0' && password.passwordArray[i] <= '9')
+                    ciphers++;
             }
-            if (smallLetters == password.nrOfSmallLetters && capitalLetters == password.nrOfCapitalLetters)
+            if (smallLetters == password.nrOfSmallLetters && capitalLetters == password.nrOfCapitalLetters && ciphers == password.nrOfCiphers)
                 return true;
             return false;
         }                
