bd0505d647adc650f09025ed56870a6696aa6ad0
CatalinAvram <avram.catalin94@yahoo.com>
2016-06-06 23:43:37 +0300

    Name and second for maximum speed

--- JuniorMind/Cyclometer/CyclometerTests.cs
+++ JuniorMind/Cyclometer/CyclometerTests.cs
@@ -27,12 +27,22 @@ namespace Cyclometer
             Assert.AreEqual(251.2, ComputeTotalDistance(cyclists), 0.1);
         }
 
+        [TestMethod]
+        public void SecondAndCyclistNameForMaximumSpeed()
+        {
+            Cyclist[] cyclists = new Cyclist[] { new Cyclist("Radu", 10, new Performances[] { new Performances(1, 1), new Performances(2, 3) }),
+                                                 new Cyclist("Vasile", 10, new Performances[] { new Performances(1, 1), new Performances(2, 6), new Performances(3, 5) }),
+                                                 new Cyclist("Marius", 10, new Performances[] { new Performances(1, 2), new Performances(2, 5) }) };
+            Assert.AreEqual(6, FindSecondOfMaximumSpeed(cyclists));
+            Assert.AreEqual("Vasile", FindNameOfFastestCyclist(cyclists));
+        }
+
         public struct Cyclist
         {
             public string cyclistName;
             public double wheelDiameter;
-            public Performances[] performances;
-
+            public Performances[] performances;    
+                  
             public Cyclist(string cyclistName, double wheelDiameter, Performances[] performances)
             {
                 this.cyclistName = cyclistName;
@@ -76,6 +86,34 @@ namespace Cyclometer
                 totalDistance += IndividualDistance(cyclists[i]);
             }
             return totalDistance;
+        }
+
+        private int FindSecondOfMaximumSpeed(Cyclist[] cyclists)
+        {
+            int secondOfMaximumSpeed = cyclists[0].performances[0].second;
+            for(int i = 0; i < cyclists.Length; i++)
+                for(int j = 0; j < cyclists[i].performances.Length; j++)
+                {
+                    if (cyclists[i].performances[j].rotations > secondOfMaximumSpeed)
+                        secondOfMaximumSpeed = cyclists[i].performances[j].rotations;
+                }
+            return secondOfMaximumSpeed;
+        }
+
+        private string FindNameOfFastestCyclist(Cyclist[] cyclists)
+        {
+            string nameOfFastestCyclist = cyclists[0].cyclistName;
+            int secondOfMaximumSpeed = cyclists[0].performances[0].second;
+            for (int i = 0; i < cyclists.Length; i++)
+                for (int j = 0; j < cyclists[i].performances.Length; j++)
+                {
+                    if (cyclists[i].performances[j].rotations > secondOfMaximumSpeed)
+                    {
+                        secondOfMaximumSpeed = cyclists[i].performances[j].rotations;
+                        nameOfFastestCyclist = cyclists[i].cyclistName;
+                    }
+                }
+            return nameOfFastestCyclist;
         }
     }
 }
