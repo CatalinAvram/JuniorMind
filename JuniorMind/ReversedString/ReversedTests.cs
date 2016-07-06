af1d13620cff66890dc4a1cab1cf53b6a9277f6e
CatalinAvram <avram.catalin94@yahoo.com>
2016-07-06 16:14:38 +0300

    Test for short string

---
+++ JuniorMind/ReversedString/ReversedTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+/*Inversează un string folosind recursivitatea*/
+
+namespace ReversedString
+{
+    [TestClass]
+    public class ReversedTests
+    {
+        [TestMethod]
+        public void OneLetter()
+        {
+            Assert.AreEqual("a", ReverseString("a"));
+        }
+
+        [TestMethod]
+        public void ShortString()
+        {
+            Assert.AreEqual("ea", ReverseString("ae"));
+        }
+        string ReverseString(string toBeReversed)
+        {
+            if (toBeReversed.Length <= 1)
+                return toBeReversed;
+            return ReverseString(toBeReversed[toBeReversed.Length - 1].ToString()) + ReverseString(toBeReversed[toBeReversed.Length - 2].ToString());                   
+        }
+    }
+}
