c87feba37288ce820566353a7ce58e5abaf0ace2
CatalinAvram <avram.catalin94@yahoo.com>
2016-07-08 11:10:28 +0300

    Two disks are moved

---
+++ JuniorMind/Calculator/CalculatorTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+/*Scrie un program care imită un calculator de buzunar în formă prefixată.
+Operațiile posibile sunt:
+    adunare/scădere
+    înmulțire/împărțire
+    suportă numere reale
+Notă: În forma prefixată operatorii apar înaintea operanzilor.
+Exemple pentru format prefixată:
+    * 3 4 e echivalent cu 3 * 4
+    * + 1 1 2, e echivalent cu (1 + 1) * 2
+    + / * + 56 45 46 3 - 1 0.25 e echivalent cu ((56 + 45) * 46) / 3 + (1 - 0.25)*/
+namespace Calculator
+{
+    [TestClass]
+    public class CalculatorTests
+    {
+        [TestMethod]
+        public void OneOperand()
+        {
+            Assert.AreEqual(7, Calculate(new char[] {'*', '3', '4'));
+        }
+
+        double Calculate(char[] charsArray)
+        {                      
+        }
+    }
+}
---
+++ JuniorMind/HanoiTowers/HanoiTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+/*Călugării dintr-un templul din Benares trebuie să mute 64 de discuri de pe un turn pe altul.
+În afară de cele două turnuri mai au la dispoziție un singur alt turn care e suficient de sacru pentru a putea fi folosit.
+Cele 64 de discuri au dimensiuni diferite.Iar călugării trebuie să respecte două reguli:
+    un singur disc poate fi mutat la un moment dat
+    un disc mai mare nu poate fi mutat peste un disc mai mic
+Notă: e o problemă clasică de recursivitate, dar care pentru 64 de discuri deja poate cauza probleme de performanță (2^64 - 1 mutări).*/
+namespace HanoiTowers
+{
+    [TestClass]
+    public class HanoiTests
+    {
+        [TestMethod]
+        public void TwoDisks()
+        {
+            Assert.AreEqual(3, HanoiTowers(2, 'S', 'A', 'D'));
+        }
+
+        int HanoiTowers(int nrOfDisks, char source, char aux, char destination)
+        {
+            if (nrOfDisks == 1)
+                return nrOfDisks;
+            return HanoiTowers(nrOfDisks - 1, source, destination, aux) +
+                    HanoiTowers(1, source, aux, destination) +
+                    HanoiTowers(nrOfDisks - 1, aux, source, destination);
+        }                                 
+    }
+}
