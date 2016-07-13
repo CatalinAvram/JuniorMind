6df10df1f07b6336d42ea990021e5ad4833edd3f
CatalinAvram <avram.catalin94@yahoo.com>
2016-07-13 11:58:45 +0300

    One operand used

--- JuniorMind/Calculator/CalculatorTests.cs
+++ JuniorMind/Calculator/CalculatorTests.cs
@@ -18,12 +18,25 @@ namespace Calculator
         [TestMethod]
         public void OneOperand()
         {
-            Assert.AreEqual(7, Calculate(new char[] { '*', '3', '4' }));
+            Assert.AreEqual(7, Calculate( "+, 3, 4", 0));
         }
 
-        double Calculate(char[] charsArray)
+        double Calculate(string expression, int  i)
         {
-            return 0;                  
+            string[] expressionElements = expression.Split(',');
+            double result;
+
+            if (double.TryParse(expressionElements[i], out result))
+                return result;
+            return SelectOperation(expression, expressionElements, i);
         }
+
+        private double SelectOperation(string expression, string[] expressionElements, int i)
+        {
+            if(expressionElements[i] == "+")                           
+                return Calculate(expressionElements[i + 1], i) + Calculate(expressionElements[i + 2], i);
+            return 0;
+        }
+
     }
 }
