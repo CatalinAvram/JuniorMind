84c09c020dab0baf9966b5733e807d5149108d8a
CatalinAvram <avram.catalin94@yahoo.com>
2016-07-19 15:45:17 +0300

    Test for a set of six drawn numbers

---
+++ JuniorMind/Loto/LotoSort.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Trebuie ca tot timpul numerele extrase la loto să fie afișate la public în ordine crescătoare
+și nu în ordinea în care au fost extrase.
+Scrie o funcție care face asta.*/
+namespace Loto
+{
+    public delegate int[] GetSortNumbersOutput(int[] drawnNumbers);
+
+    class LotoSort
+    {
+        private int[] SortNumbers(int[] drawnNumbers)
+        {
+            for (int i = 0; i < drawnNumbers.Length - 1; i++)
+            {
+                int j = i + 1;
+                while (j > 0)
+                {
+                    if (drawnNumbers[j - 1] > drawnNumbers[j])
+                    {
+                        int aux = drawnNumbers[j - 1];
+                        drawnNumbers[j - 1] = drawnNumbers[j];
+                        drawnNumbers[j] = aux;
+                    }
+                    j--;
+                }
+            }
+            return drawnNumbers;
+        }
+            
+        public GetSortNumbersOutput SortNumbersOutput
+        {
+            get
+            {
+                return new GetSortNumbersOutput(SortNumbers);
+            }
+        }
+    }
+}
---
+++ JuniorMind/Loto/LotoTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Loto
+{
+    [TestClass]
+    public class LotoTests
+    {
+        [TestMethod]
+        public void SixNumbersSet()
+        {
+            GetSortNumbersOutput SortNumbers = null;
+            LotoSort ob = new LotoSort();
+            SortNumbers = new GetSortNumbersOutput(ob.SortNumbersOutput);           
+            int[] drawnNumbers = new int[] { 22, 31, 4, 9, 48, 28 };
+            CollectionAssert.AreEqual(new int[] { 4, 9, 22, 28, 31, 48 }, SortNumbers(drawnNumbers));
+        }
+    }
+}
