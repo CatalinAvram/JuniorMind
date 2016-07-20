cc713de3d0c33d07e5c3929cb969cfe4348c316d
CatalinAvram <avram.catalin94@yahoo.com>
2016-07-20 21:21:27 +0300

    Test for 3 cars

---
+++ JuniorMind/RepairCenter/RepairCenterTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+/*La un centru de reparații fiecare caz are o prioritate: High, Medium și Low.
+Ordonează cazurile în funcție de prioritate.*/
+namespace RepairCenter
+{
+    [TestClass]
+    public class RepairCenterTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SortCases ob = new SortCases();
+            Car[] carsToBeSorted = new Car[3];
+            for (int i = 0; i < carsToBeSorted.Length; i++)
+            {
+                carsToBeSorted[i] = new Car();
+            }
+            carsToBeSorted[0].Priority = "Medium";
+            carsToBeSorted[1].Priority = "High";
+            carsToBeSorted[2].Priority = "Low";
+            CollectionAssert.AreEqual(new Car[] { carsToBeSorted[1], carsToBeSorted[0], carsToBeSorted[2] }, ob.SortAfterPriority(carsToBeSorted));
+        }
+    }
+}
---
+++ JuniorMind/RepairCenter/SortCases.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+/*La un centru de reparații fiecare caz are o prioritate: High, Medium și Low.
+Ordonează cazurile în funcție de prioritate.*/
+namespace RepairCenter
+{
+    public class SortCases
+    {
+        public Car[] SortAfterPriority(Car[] carsToBeSorted)
+        {
+            Car carObject = new Car();
+            //int[] finalOrder = new int[carsToBeSorted.Length];
+            string priorities = "HighMediumLow";    
+             
+            for(int i = 0; i <= carsToBeSorted.Length - 1; i++)
+                for(int j = i + 1; j < carsToBeSorted.Length; j++)
+                {
+                    if (priorities.IndexOf(carsToBeSorted[i].Priority) > priorities.IndexOf(carsToBeSorted[j].Priority))
+                    {
+                        carObject = carsToBeSorted[i];
+                        carsToBeSorted[i] = carsToBeSorted[j];
+                        carsToBeSorted[j] = carObject;
+                    }                       
+                }
+            return carsToBeSorted;                               
+        }
+    }
+
+    public class Car
+    {
+        private string priority;
+        public string Priority
+        {
+            get { return priority; }
+            set { priority = value; }
+        }                 
+    }
+}
