78c78067cbf4824bd1ca8ef54e9a0e17d4fa1a51
CatalinAvram <avram.catalin94@yahoo.com>
2016-08-23 17:30:39 +0300

    Test for count method

---
+++ JuniorMind/IListImplementation/IListTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace IListImplementation
+{
+    [TestClass]
+    public class IListTests
+    {
+        [TestMethod]
+        public void CountTest()
+        {
+            var integersList = new List<int>();
+            Assert.AreEqual(0, integersList.Count);
+        }     
+    }
+}
---
+++ JuniorMind/IListImplementation/List.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IListImplementation
+{
+    class List<T> : IList<T>
+    {
+        private T[] myList = new T[0];
+        private int count = 0;
+
+        public T this[int index]
+        {
+            get
+            {
+                return myList[index];
+            }
+
+            set
+            {
+                myList[index] = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+               return count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int IndexOf(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(int index, T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
